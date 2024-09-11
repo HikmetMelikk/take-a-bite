f6e326a86da0cb0991c3ffacc7eb5c7f6d45bfbb
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-09-11 23:58:25 +0300

    Take A Bite repository created

---
+++ android/app/src/main/kotlin/com/example/take_a_bite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.take_a_bite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/feature/home/view/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+part 'widget/home_app_bar.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: _HomeAppBar(),
+      body: Placeholder(),
+    );
+  }
+}
