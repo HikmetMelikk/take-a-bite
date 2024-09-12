87bce350626ae06e46df937cdc4382540d1e9378
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-09-13 00:47:21 +0300

    -Firebase integrated to application -Update iOS and macOS configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:take_a_bite/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
