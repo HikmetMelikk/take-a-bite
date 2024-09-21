82cd9039a9e344fda52927ba609afb42ce50d7e4
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-09-21 21:34:01 +0300

    home view products widget

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -54,7 +54,7 @@ class _HomeViewState extends State<HomeView> {
               margin: const EdgeInsets.symmetric(vertical: 20.0),
               height: 50.0,
               child: ListView.builder(
-                scrollDirection: Axis.horizontal, // Yatay kaydırma
+                scrollDirection: Axis.horizontal,
                 itemCount: categories.length,
                 itemBuilder: (context, index) => GestureDetector(
                   onTap: () {
@@ -86,26 +86,64 @@ class _HomeViewState extends State<HomeView> {
                 ),
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10, // Replace with the actual number of items
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: PageView.builder(
+                itemCount: 10,
                 itemBuilder: (context, index) {
-                  return Container(
-                    width: 160.0,
-                    margin: const EdgeInsets.only(right: 16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
+                  return Stack(
+                    children: [
+                      Center(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.68,
+                          height: MediaQuery.of(context).size.height * 0.35,
+                          margin: const EdgeInsets.only(top: 80),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color(0xFF000000).withOpacity(0.1),
+                                blurRadius: 9.0,
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Spicy seasoned seafood noodles",
+                                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text("\$2.99", style: Theme.of(context).textTheme.bodyMedium),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: NetworkImage("https://picsum.photos/200"),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   );
                 },
               ),
